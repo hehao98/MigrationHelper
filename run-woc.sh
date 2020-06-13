ccd794e2efaffae75a1c0008f5bb5d520d7e9f1d
Hao He <heh@pku.edu.cn>
2020-06-13 15:02:18 +0800

    Updated running script

--- run-woc.sh
+++ run-woc.sh
@@ -1,9 +1,15 @@
+if [ "$1" != "DataExportJob" ] && [ "$1" != "LibraryRecommendJob" ] && [ "$1" != "+" ]; then
+  echo "Usage: run-woc.sh <Job Name>"
+  exit
+fi
+echo "Pulling Latest Version..."
 cd MigrationHelperJAR || exit
 git pull origin master
 cd ..
+echo "MigrationHelper: Running Job $1..."
 /home/heh/jdk1.8.0_144/bin/java -Djava.library.path=/home/heh/lib \
      -Dlog4j.configuration=mylog4j.properties \
      -Dspoon.log.path=./spoon.log \
      -Dspring.profiles.active=woc \
-     -Dmigration-helper.job.enabled=LibraryRecommendJob \
+     -Dmigration-helper.job.enabled=$1 \
      -jar MigrationHelperJAR/migration-helper-1.0-SNAPSHOT.jar
