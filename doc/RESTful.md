30aa1ac818049640b8e1e6fb91cd3bbdc24cc846
Hao He <heh@pku.edu.cn>
2020-10-23 10:29:46 +0800

    Finished Error Handling for RESTful Backend

--- doc/RESTful.md
+++ doc/RESTful.md
@@ -2,9 +2,32 @@
 
 Currently, I do not have time to document the behavior of each API in detail. However, you can quickly checkout what is returned using the following example links.
 
+## Error Message Example
+
+```
+http://migration-helper.net:22568/recommend?fromLib=something:non-existent&pageNum=0&pageSize=20
+```
+```json
+{
+  "timestamp": "2020-10-23T02:28:34.015+0000",
+  "status": 404,
+  "error": "404 Not Found",
+  "message": "fromLib something:non-existent does not exist",
+  "path": "/recommend",
+  "url": "http://migration-helper.net:22568/recommend?fromLib=something:non-existent&pageNum=0&pageSize=20"
+}
+```
+
+
 ## Query Migration Recommendations for a Library
 
 ```
 http://migration-helper.net:22568/recommend?fromLib=org.json:json&pageNum=0&pageSize=20
+```
+
+## Query Related Repositories and Commits for a Source/Target Library Pair
+
+```
+http://migration-helper.net:22568/recommend-one?fromLib=org.json:json&toLib=com.google.code.gson:gson
 ```
 
--- src/main/java/edu/pku/migrationhelper/controller/RestResponseEntityExceptionHandler.java
+++ src/main/java/edu/pku/migrationhelper/controller/RestResponseEntityExceptionHandler.java
@@ -1,33 +1,55 @@
 package edu.pku.migrationhelper.controller;
 
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@ControllerAdvice
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@RestControllerAdvice
 public class RestResponseEntityExceptionHandler
         extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(value
-            = { IllegalArgumentException.class, IllegalStateException.class })
-    protected ResponseEntity<Object> handleConflict(
-            RuntimeException ex, WebRequest request) {
-        /*
-         "timestamp": "2019-01-17T16:12:45.977+0000",
-         "status": 500,
-         "error": "Internal Server Error",
-         "exception":
-         "message": "Error processing the request!",
-         "path": "/my-endpoint-with-exceptions"
-         */
-        //Map<String, Object> response = new HashMap<>();
-        //response.put("timestamp", Date.)
-        String bodyOfResponse = "This should be application specific";
-        return handleExceptionInternal(ex, bodyOfResponse,
+    @ExceptionHandler(value = { IllegalArgumentException.class })
+    protected ResponseEntity<Object> handleIllegalArgument(
+            RuntimeException ex, ServletWebRequest request) {
+        Map<String, Object> response = getErrorResponse(
+                HttpStatus.BAD_REQUEST, ex.getMessage(),
+                request.getRequest().getRequestURI(),
+                request.getRequest().getRequestURL().toString() + "?" + request.getRequest().getQueryString()
+        );
+        return handleExceptionInternal(ex, response,
+                new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    }
+
+    @ExceptionHandler(value = { ResourceNotFoundException.class })
+    protected ResponseEntity<Object> handleResourceNotFound(
+            RuntimeException ex, ServletWebRequest request
+    ) {
+        Map<String, Object> response = getErrorResponse(
+                HttpStatus.NOT_FOUND, ex.getMessage(),
+                request.getRequest().getRequestURI(),
+                request.getRequest().getRequestURL().toString() + "?" + request.getRequest().getQueryString()
+        );
+        return handleExceptionInternal(ex, response,
                 new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    }
+
+    private Map<String, Object> getErrorResponse(HttpStatus status, String message, String path, String url) {
+        Map<String, Object> response = new LinkedHashMap<>();
+        response.put("timestamp", new Date());
+        response.put("status", status.value());
+        response.put("error", status.value() + " " + status.getReasonPhrase());
+        response.put("message", message);
+        response.put("path", path);
+        response.put("url", url);
+        return response;
     }
 }
