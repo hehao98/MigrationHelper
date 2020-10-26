93a916a3d420bc850dec5b755f464125f075590a
Hao He <heh@pku.edu.cn>
2020-10-26 11:17:20 +0800

    Allow cross origin requests

--- src/main/java/edu/pku/migrationhelper/controller/MigrationRecommendationController.java
+++ src/main/java/edu/pku/migrationhelper/controller/MigrationRecommendationController.java
@@ -15,6 +15,7 @@ import org.springframework.hateoas.Link;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
