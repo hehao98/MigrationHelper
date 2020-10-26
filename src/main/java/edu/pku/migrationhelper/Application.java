207235377395cc82ee3322ca79efca7859a0ede3
Hao He <heh@pku.edu.cn>
2020-10-26 11:17:20 +0800

    Allow cross origin requests

--- src/main/java/edu/pku/migrationhelper/Application.java
+++ src/main/java/edu/pku/migrationhelper/Application.java
@@ -5,6 +5,9 @@ import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.transaction.TransactionAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(exclude = {
         DataSourceAutoConfiguration.class,
@@ -22,5 +25,15 @@ public class Application {
             springApplication.setWebApplicationType(WebApplicationType.NONE);
             springApplication.run(args);
         }
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:8080");
+            }
+        };
     }
 }
